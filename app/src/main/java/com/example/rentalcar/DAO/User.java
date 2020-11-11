63be5fb2c0690fecd1d232a94bc80f01b3cc2a46
Minh Tân <58105178+PhamNhatMinhTan@users.noreply.github.com>
2020-11-11 14:58:54 +0700

    Create User DAO

---
+++ app/src/main/java/com/example/rentalcar/DAO/User.java
@@ -0,0 +1,24 @@
+package com.example.rentalcar.DAO;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class User extends SQLiteOpenHelper {
+
+    public User(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
